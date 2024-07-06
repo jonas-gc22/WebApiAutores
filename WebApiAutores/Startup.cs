a3f2aa3b8726cc299b57c0db71376138c7ef0eac
Jonas <usuario@MacBook-Pro-de-Usuario.local>
2024-07-06 00:53:30 -0400

    Changes in project, new connection DATABASE

--- WebApiAutores/Startup.cs
+++ WebApiAutores/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiAutores
 {
@@ -13,7 +14,10 @@ namespace WebApiAutores
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(x =>
+               x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
             //options.UseSqlServer(Configuration.GetConnectionString("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CursoWebApis;Integrated Security=True")));
